25641d40a0ceb3834621c5f8f4b09c55badab8d4
KimNikita <ghj710@mail.ru>
2022-03-22 21:59:47 +0300

    full pipeline

--- src/main.py
+++ src/main.py
@@ -2,21 +2,20 @@ import sys
 import os
 import functools
 import qimage2ndarray
-import design  # Это наш конвертированный файл дизайна
+import design
+import math
+import tarfile
+import tifffile as tiff
 import numpy as np
 import cv2 as cv
-import tarfile
 
 from PyQt5 import QtCore, QtGui, QtWebEngineWidgets, QtWebChannel
 from PyQt5.QtWidgets import QDialog, QLineEdit, QDialogButtonBox, QLabel, QFormLayout, QMainWindow, QMessageBox, QApplication
 from landsatxplore.api import API
+from landsatxplore.errors import EarthExplorerError
 from landsatxplore.earthexplorer import EarthExplorer
 from models.kumar_roy import KumarRoy64_10
 from models.cloud_net import CloudNet
-
-# Костыль
-from skimage.io import imread
-from skimage.transform import resize
 
 
 class SignIn(QDialog):
@@ -43,6 +42,40 @@ class SignIn(QDialog):
     def getLP(self):
         return self.login.text(), self.password.text()
 
+# TODO finish choosing date
+class Date(QDialog):
+    def __init__(self, start, end):
+        super(SignIn, self).__init__()
+
+        # does it need to store?
+        self.start = ''
+        self.end = ''
+        if start:
+            self.start = start
+        if end:
+            self.end = end
+
+        # listbox???
+        self.start_date = None
+        self.end_date = None
+
+        buttonBox = QDialogButtonBox(
+            QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
+        label = QLabel(self)
+        label.setText("Choose start and end date")
+
+        layout = QFormLayout(self)
+        layout.addWidget(label)
+        # add dates
+        layout.addWidget(buttonBox)
+
+        buttonBox.accepted.connect(self.accept)
+        buttonBox.rejected.connect(self.reject)
+
+    def getDate(self):
+        # return properly
+        return None, None
+
 
 class SatelliteApp(QMainWindow, design.Ui_MainWindow):
     api = None
@@ -50,15 +83,13 @@ class SatelliteApp(QMainWindow, design.Ui_MainWindow):
     # from earth explorer site https://ers.cr.usgs.gov
     username = ''
     password = ''
-    # date - string
-    year = None
-    month = None
-    day = None
-    # loaded/choosen tiff image
+    start_date = None
+    end_date = None
     image = None
     models = []
     labels = []
     runs = 0
+    online = True
 
     def __init__(self):
         super().__init__()
@@ -69,8 +100,10 @@ class SatelliteApp(QMainWindow, design.Ui_MainWindow):
         self.fire = KumarRoy64_10('C://Users//Никита//Desktop//fire//model.h5')
         self.cloud = CloudNet('C://Users//Никита//Desktop//cloud//model.h5')
 
-        self.models.append(self.fire)
-        self.models.append(self.cloud)
+        # TODO relocate short names "fire" & "cloud" to a model classes as methods like CloudNet.get_type()
+        # and with that, fix self.save() method
+        self.models.append((self.fire, 'fire'))
+        self.models.append((self.cloud, 'cloud'))
 
         # Map
 
@@ -93,8 +126,8 @@ class SatelliteApp(QMainWindow, design.Ui_MainWindow):
         self.preview = QLabel(self)
         self.ispreview = False
         self.button_preview.clicked.connect(self.preview_mode)
-        self.button_date.clicked.connect(self.choose_date)
-        self.button_clear.clicked.connect(self.clear_markers)
+        self.button_mode.clicked.connect(self.mode)
+        self.button_clear.clicked.connect(self.clear_marker)
 
         # Buttons
 
@@ -107,7 +140,7 @@ class SatelliteApp(QMainWindow, design.Ui_MainWindow):
         self.labels.append(self.label_fire)
         self.labels.append(self.label_cloud)
 
-        # Sign In
+        # Sign in
 
         self.sign_in = SignIn()
 
@@ -122,12 +155,12 @@ class SatelliteApp(QMainWindow, design.Ui_MainWindow):
         return self.num_markers
 
     def norm_markers(self, x, y):
-        left_up = (min(x[0], y[0]), min(x[1], y[1]))
-        right_down = (max(x[0], y[0]), max(x[1], y[1]))
-        return left_up, right_down
+        ul = (min(x[0], y[0]), min(x[1], y[1]))
+        lr = (max(x[0], y[0]), max(x[1], y[1]))
+        return ul, lr
 
     # Map Properties
-
+    # TODO finish preview_mode
     def preview_mode(self):
         if self.image is None:
             return
@@ -137,7 +170,8 @@ class SatelliteApp(QMainWindow, design.Ui_MainWindow):
             # не работает так как надо, хмм...
             self.gridLayout_2.removeWidget(self.view)
             # работает только на тестовом изображении для fire detection!!!!!
-            convert = np.concatenate((self.image[:,:,7:8],self.image[:,:,6:7],self.image[:,:,2:3]), axis = 2)
+            convert = np.concatenate(
+                (self.image[:, :, 7:8], self.image[:, :, 6:7], self.image[:, :, 2:3]), axis=2)
             convert = qimage2ndarray.array2qimage(convert)
             self.preview.setPixmap(QtGui.QPixmap.fromImage(convert))
             self.gridLayout_2.addWidget(self.preview)
@@ -150,71 +184,105 @@ class SatelliteApp(QMainWindow, design.Ui_MainWindow):
             self.gridLayout_2.addWidget(self.view)
             self.ispreview = True
 
-    def choose_date(self):
-        # debug
-        return
-        # create new form/dialog to choose year, month and day as listbox/input_text/something
+    def mode(self):
+        if self.online == True:
+            self.online = False
+            self.button_mode.setText("Online mode")
+            # TODO implement ofline mode
+        else:
+            self.online = True
+            self.button_mode.setText("Offline mode")
 
-    def clear_markers(self):
-        self.num_markers = 0
-        self.markers[0] = (None, None)
-        self.markers[1] = (None, None)
-        self.view.page().runJavaScript("map.removeLayer(marker_1);")
-        self.view.page().runJavaScript("map.removeLayer(marker_2);")
+    def clear_marker(self):
+        if self.num_markers == 2:
+            self.markers[1] = (None, None)
+            self.num_markers = 1
+            self.view.page().runJavaScript("map.removeLayer(marker_2);")
+        elif self.num_markers == 1:
+            self.markers[0] = (None, None)
+            self.num_markers = 0
+            self.view.page().runJavaScript("map.removeLayer(marker_1);")
 
     # Buttons
 
     def analyze(self):
-        if self.num_markers != 2:
-            self.view.page().runJavaScript("alert_markers();")
-            return
-        if not self.authorize():
-            return
-        # координаты левого верхнего и правого нижнего угла изображения (lat, lng)
-        upleft, lowright = self.norm_markers(self.markers[0], self.markers[1])
-        # TODO заюзать нормальный код из landsat_demo (сделать отдельный класс?)
-        # найти нужное изображение
-        scenes = self.api.search(
-            dataset='landsat_8_c1',
-            #latitude=(lowright[0]+upleft[0])/2,
-            #longitude=(lowright[1]+upleft[1])/2,
-            bbox=(upleft[0], upleft[1], lowright[0], lowright[1]),
-            start_date='2021-01-01',
-            end_date='2021-03-01',
-            max_cloud_cover=10  # hz how much is needed
-        )
-        # загрузить на комп
-        # self.ee.download(
-            # identifier=scenes[0]['display_id'], output_dir='./downloaded')
-        # прочитать с компа в self.image
-        image_name = scenes[0]['display_id']
-        # Костыль
-        images_path = 'C:\\mygit\\ITLab\\satellite_images_processing\\src\\downloaded\\'
-        images = []
-        size = None
-        ''' закомменчу пока, так как работает долговато
-        tar = tarfile.open(images_path+image_name+'.tar.gz', 'r')
+        if self.online == True:
+            if self.num_markers != 2:
+                self.view.page().runJavaScript("alert_markers();")
+                return
+            result = self.authorize()
+            if result == False:
+                return
+            self.date = Date(self.start_date, self.end_date)
+            self.date.exec_()
+            self.start_date, self.end_date = self.date.getDate()
 
-        for i in range(10):
-            tar.extract(image_name + '_B{}.TIF'.format(i+1), images_path+image_name)
-            image = imread(images_path+image_name + '\\' +
-                           image_name + '_B{}.TIF'.format(i+1))
-            if(i == 0):
-                size = image.shape
-            # ландсат выдает снимки разного размера
-            image = resize(image, size, preserve_range=True, mode='symmetric')
-            # предобработать картинки к рабочему формату
+            # from landsat_downloader
+            ul, lr = self.norm_markers(self.markers[0], self.markers[1])
+            bbox = (ul[0], ul[1], lr[0], lr[1])
+            scenes = self.api.search(
+                dataset='landsat_8_c1',
+                bbox=bbox,
+                start_date=self.start_date,
+                end_date=self.end_date,
+                max_cloud_cover=10,
+                max_results=1
+            )
+            image = scenes[0]['display_id']
+            zip_path = os.path.abspath("")+'\\landsat_downloaded\\zips\\'
+            images_path = zip_path[:-5]
+            try:
+                self.ee.download(identifier=image,
+                                 output_dir=zip_path, dataset='landsat_8_c1')
+                tar = tarfile.open(zip_path+image+'.tar.gz', 'r')
+                tar.extractall(path=images_path+image)
+            except EarthExplorerError as e:
+                print(e, end='')
+                print(' for ' + image + ' from dataset landsat_8_c1')
 
-            images.append(image)
-        '''
-        # self.image = np.stack((images[0], images[1], images[2], images[3], images[4],
-            # images[5], images[6], images[7], images[8], images[9]), axis=-1)
-        # Костыль
-        self.image = imread('C://Users//Никита//Desktop//fire//image.tif')
+            # from cropper_demo
+            with open(images_path+image+'_MTL.txt', 'r') as f:
+                data = f.read().split('PRODUCT_METADATA')[1].split('\n')[14:22]
+            meta = {}
+            for info in data:
+                meta[info.split('=')[0][4:-1]] = float(info.split('=')[1][1:])
+
+            # lattitude = y, longitude = x on image
+            ul = (meta['CORNER_UL_LON_PRODUCT'], meta['CORNER_UL_LAT_PRODUCT'])
+            ur = (meta['CORNER_UR_LON_PRODUCT'], meta['CORNER_UR_LAT_PRODUCT'])
+            ll = (meta['CORNER_LL_LON_PRODUCT'], meta['CORNER_LL_LAT_PRODUCT'])
+            lr = (meta['CORNER_LR_LON_PRODUCT'], meta['CORNER_LR_LAT_PRODUCT'])
+            x1_l, y1_l = ll[0], ul[1]
+            x2_l, y2_l = ur[0], lr[1]
+
+            channels = []
+            for i in range(10):
+                img = tiff.imread(images_path+image+'_B{}.TIF'.format(i+1))
+                x_scale = abs(x2_l-x1_l)/img.shape[1]
+                y_scale = abs(y2_l-y1_l)/img.shape[0]
+                x1_b = abs(bbox[1]-x1_l)/x_scale
+                x2_b = abs(bbox[3]-x1_l)/x_scale
+                y1_b = abs(bbox[0]-y1_l)/y_scale
+                y2_b = abs(bbox[2]-y1_l)/y_scale
+                x1_b, x2_b = math.floor(
+                    min(x1_b, x2_b)), math.ceil(max(x1_b, x2_b))
+                y1_b, y2_b = math.floor(
+                    min(y1_b, y2_b)), math.ceil(max(y1_b, y2_b))
+                if i == 0:
+                    size = (x2_b-x1_b, y2_b-y1_b)
+                channels.append(cv.resize(
+                    img[y1_b:y2_b+1, x1_b:x2_b+1], size, interpolation=cv.INTER_AREA))
+
+            self.image = np.stack((channels[0], channels[1], channels[2], channels[3], channels[4],
+                                   channels[5], channels[6], channels[7], channels[8], channels[9]), axis=-1)
+        else:
+            # Костыль
+            self.image = tiff.imread(
+                'C://Users//Никита//Desktop//fire//image.tif')
 
         i = 0
         for model in self.models:
-            res = model.process(self.image)
+            res = model[0].process(self.image)
             res = qimage2ndarray.array2qimage(res)
             self.labels[i].setPixmap(QtGui.QPixmap.fromImage(res))
             i += 1
@@ -223,7 +291,7 @@ class SatelliteApp(QMainWindow, design.Ui_MainWindow):
         i = 0
         for label in self.labels:
             img = qimage2ndarray.rgb_view(label.pixmap().toImage())
-            cv.imwrite('res_model_{}_{}.png'.format(i, self.runs),
+            cv.imwrite('res_{}_model_{}.png'.format(self.models[i][1], self.runs),
                        cv.cvtColor(img, cv.COLOR_RGB2BGR))
             i += 1
         self.runs += 1
@@ -233,13 +301,12 @@ class SatelliteApp(QMainWindow, design.Ui_MainWindow):
                                      QMessageBox.Yes | QMessageBox.No,
                                      QMessageBox.No)
         if reply == QMessageBox.Yes:
-            self.ee.logout()
-            self.api.logout()
+            if self.ee:
+                self.ee.logout()
+                self.api.logout()
             self.close()
 
-    # Results
-
-    # Sign In
+    # Sign in
 
     def authorize(self):
         if self.username == '' or self.password == '':
@@ -247,9 +314,11 @@ class SatelliteApp(QMainWindow, design.Ui_MainWindow):
             self.username, self.password = self.sign_in.getLP()
             if self.username == '' or self.password == '':
                 return False
-            # добавить try catch блок
-            self.api = API(self.username, self.password)
-            self.ee = EarthExplorer(self.username, self.password)
+            try:
+                self.api = API(self.username, self.password)
+                self.ee = EarthExplorer(self.username, self.password)
+            except Exception as e:
+                print(e)
         return True
 
 
