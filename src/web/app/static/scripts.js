6965965de2bffccd8220a9ecc417721064de5298
KimNikita <ghj710@mail.ru>
2022-04-26 16:30:54 +0300

    added map

--- src/web/app/static/scripts.js
+++ src/web/app/static/scripts.js
@@ -1,19 +1,68 @@
+var marker_1;
+var marker_2;
+var num = 0;
+var map;
+
+function onMapClick(e) {
+    if (num == 0) {
+        marker_1 = L.marker(e.latlng).addTo(map);
+        num = 1;
+    } else if (num == 1) {
+        marker_2 = L.marker(e.latlng).addTo(map);
+        num = 2;
+    } else {
+        alert("Too many markers");
+    }
+}
+
+function createMap() {
+
+    var mapOptions = {
+        center: [56.29827, 43.98141],
+        zoom: 10
+    }
+
+    map = new L.map('map', mapOptions);
+
+    var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+
+    map.addLayer(layer);
+
+    map.on('click', onMapClick)
+}
+
+function clearMarker() {
+    if (num == 2) {
+        num = 1;
+        map.removeLayer(marker_2);
+    }
+    else if (num == 1) {
+        num = 0;
+        map.removeLayer(marker_1);
+    }
+}
 
 // кнопка анализа берет маркеры с карты и дату с селектора
-function analyze(marker_1, marker_2, date) {
-    $.ajax({
-        url: "/analyze/",
-        type: "POST",
-        contentType: "application/json",
-        data: JSON.stringify({'marker_1': marker_1, 'marker_2': marker_2, 'date': date}),
-        // for debug
-        success: function (data) {
-            alert(data);
-        },
-    });
+function analyze(date) {
+    if (num != 2) {
+        alert("Use 2 markers to define area to analyze")
+    }
+    else {
+        $.ajax({
+            url: "/analyze/",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({ 'marker_1': [marker_1.getLatLng().lat, marker_1.getLatLng().lng], 'marker_2': [marker_2.getLatLng().lat, marker_2.getLatLng().lng], 'date': date }),
+            // for debug
+            success: function (data) {
+                alert(data);
+            },
+        });
+    }
 }
 
 // наверно для выбора статистики (зависит от реализации)
 function select(stat) {
-    
+
 }
+
--- src/web/app/views.py
+++ src/web/app/views.py
@@ -4,20 +4,26 @@ from app import app
 from datetime import date
 
 # генерация главной страницы
+
 @app.route('/')
 def main():
     today = date.today()
-    return render_template("index.html", today = today.strftime("%Y-%m-%d"))
+    return render_template("index.html", today=today.strftime("%Y-%m-%d"))
 
 # сюда будут приходить http запросы, генерируемые javascript скриптами
+
 @app.route('/analyze/', methods=['POST'])
 def analyze():
     data = request.get_json(silent=True)
+    marker_1 = (data['marker_1'][0], data['marker_1'][1])
+    marker_2 = (data['marker_2'][0], data['marker_2'][1])
+    date = data['date']
     # find, download, process,
     # calculate statistic & save statistic to database
-    print(data)
-    print(data["date"])
+    print(marker_1, marker_2, date)
+    # debug
     return "analyze completed successfully"
+
 
 @app.route('/show/', methods=['POST'])
 def show():
